89574838339cda78ea5e6d5c058b6c9a09e6f904
imvee <vind.veeru@gmail.com>
2023-07-07 22:20:50 +0530

    Building streamlit app

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
-st.title("Hello World")
+st.title("🥣CulinaryCrafter")
 
 cuisine=st.sidebar.selectbox('Pick a cuisine',('Indian (South Indian and North Indian)','South Indian', 'North Indian', 'Italian', 'French', 'Chinese', 'Japanese', 'Spanish', 'Lebanese', 'Mexican', 'Arabic', 'American','Thain','Turkey'))
 res_type=st.sidebar.radio('Choose a diet',("Vegetarian","Non-Vegetarian","Vegan"))
 location=st.sidebar.text_input('Type the name of the city')
+
 
